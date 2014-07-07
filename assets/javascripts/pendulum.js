71439700e5075a90d18ebe2f4b876eed377a8f05
Luis Batalha <luisbatalha10@gmail.com>
2014-07-06 19:17:47 -0700

    Changes phi to theta

--- assets/javascripts/pendulum.js
+++ assets/javascripts/pendulum.js
@@ -3,12 +3,12 @@ $('#set_variables_form').submit(function (e) {
   console.log($('#mass1').val());
   m1     = $('#mass1').val();
   m2     = $('#mass2').val();
-  Phi1   = $('#Phi1').val()/180*(Math.PI);
-  Phi2   = $('#Phi2').val()/180*(Math.PI);
-  d2Phi1 = 0;
-  d2Phi2 = 0;
-  dPhi1  = 0;
-  dPhi2  = 0;
+  Theta1   = $('#Theta1').val()/180*(Math.PI);
+  Theta2   = $('#Theta2').val()/180*(Math.PI);
+  d2Theta1 = 0;
+  d2Theta2 = 0;
+  dTheta1  = 0;
+  dTheta2  = 0;
   run();
 });
 
@@ -32,17 +32,17 @@ function drawLine(myLine, context) {
 
 function animate(myCircle1, myCircle2, myLine1, myLine2, canvas, context) {
   mu      =  1+m1/m2;
-  d2Phi1  =  (g*(Math.sin(Phi2)*Math.cos(Phi1-Phi2)-mu*Math.sin(Phi1))-(l2*dPhi2*dPhi2+l1*dPhi1*dPhi1*Math.cos(Phi1-Phi2))*Math.sin(Phi1-Phi2))/(l1*(mu-Math.cos(Phi1-Phi2)*Math.cos(Phi1-Phi2)));
-  d2Phi2  =  (mu*g*(Math.sin(Phi1)*Math.cos(Phi1-Phi2)-Math.sin(Phi2))+(mu*l1*dPhi1*dPhi1+l2*dPhi2*dPhi2*Math.cos(Phi1-Phi2))*Math.sin(Phi1-Phi2))/(l2*(mu-Math.cos(Phi1-Phi2)*Math.cos(Phi1-Phi2)));
-  dPhi1   += d2Phi1*time;
-  dPhi2   += d2Phi2*time;
-  Phi1    += dPhi1*time;
-  Phi2    += dPhi2*time;
+  d2Theta1  =  (g*(Math.sin(Theta2)*Math.cos(Theta1-Theta2)-mu*Math.sin(Theta1))-(l2*dTheta2*dTheta2+l1*dTheta1*dTheta1*Math.cos(Theta1-Theta2))*Math.sin(Theta1-Theta2))/(l1*(mu-Math.cos(Theta1-Theta2)*Math.cos(Theta1-Theta2)));
+  d2Theta2  =  (mu*g*(Math.sin(Theta1)*Math.cos(Theta1-Theta2)-Math.sin(Theta2))+(mu*l1*dTheta1*dTheta1+l2*dTheta2*dTheta2*Math.cos(Theta1-Theta2))*Math.sin(Theta1-Theta2))/(l2*(mu-Math.cos(Theta1-Theta2)*Math.cos(Theta1-Theta2)));
+  dTheta1   += d2Theta1*time;
+  dTheta2   += d2Theta2*time;
+  Theta1    += dTheta1*time;
+  Theta2    += dTheta2*time;
 
-  myCircle1.x = X0+l1*Math.sin(Phi1);
-  myCircle1.y = Y0+l1*Math.cos(Phi1);
-  myCircle2.x = X0+l1*Math.sin(Phi1)+l2*Math.sin(Phi2);
-  myCircle2.y = Y0+l1*Math.cos(Phi1)+l2*Math.cos(Phi2);
+  myCircle1.x = X0+l1*Math.sin(Theta1);
+  myCircle1.y = Y0+l1*Math.cos(Theta1);
+  myCircle2.x = X0+l1*Math.sin(Theta1)+l2*Math.sin(Theta2);
+  myCircle2.y = Y0+l1*Math.cos(Theta1)+l2*Math.cos(Theta2);
 
   myLine1.x  = myCircle1.x;
   myLine1.y  = myCircle1.y;
@@ -60,12 +60,12 @@ function animate(myCircle1, myCircle2, myLine1, myLine2, canvas, context) {
 }
 
 //Physics Constants
-var d2Phi1 = 0;
-var d2Phi2 = 0;
-var dPhi1  = 0;
-var dPhi2  = 0;
-var Phi1   = 0*(Math.PI)/2;
-var Phi2   = 2.3*(Math.PI)/2;
+var d2Theta1 = 0;
+var d2Theta2 = 0;
+var dTheta1  = 0;
+var dTheta2  = 0;
+var Theta1   = 0*(Math.PI)/2;
+var Theta2   = 2.3*(Math.PI)/2;
 var m2     = 10;
 var l1     = 150;
 var l2     = 150;
@@ -81,8 +81,8 @@ var init    = {};
 function run(){
   var myLine1 = {x0: X0, y0: Y0, x: 0, y: 0};
   var myLine2 = {x0: 0, y0: 0, x: 0, y: 0};
-  var myCircle1 = {x: X0+l1*Math.sin(Phi1), y: Y0+l1*Math.cos(Phi1), mass: m1};
-  var myCircle2 = {x: X0+l1*Math.sin(Phi1)+l2*Math.sin(Phi2), y: Y0+l1*Math.cos(Phi1)+l2*Math.cos(Phi2), mass: m2};
+  var myCircle1 = {x: X0+l1*Math.sin(Theta1), y: Y0+l1*Math.cos(Theta1), mass: m1};
+  var myCircle2 = {x: X0+l1*Math.sin(Theta1)+l2*Math.sin(Theta2), y: Y0+l1*Math.cos(Theta1)+l2*Math.cos(Theta2), mass: m2};
 
   clearInterval(init);
   context.clearRect(0, 0, canvas.width, canvas.height);
